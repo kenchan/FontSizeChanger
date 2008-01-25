a53a307bb473e4e4c93b861f124f8accf08a01aa
kenichi_taka <kenichi_taka@a84ad68e-143c-0410-beb8-9681d4ff767e>
2008-01-25 00:23:18 +0000

    Bug Fix
    git-svn-id: https://eclipse-study.svn.sourceforge.net/svnroot/eclipse-study/EasyFontChanger/tags/REL-1.1.1@166 a84ad68e-143c-0410-beb8-9681d4ff767e

---
+++ net.shu-cream.eclipse.font/src/net/shu_cream/eclipse/font/ResetHandler.java
@@ -0,0 +1,16 @@
+package net.shu_cream.eclipse.font;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+public class ResetHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+		FontPlugin.getDefault().reset();
+		return null;
+	}
+
+}
